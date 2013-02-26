a76503199cdb3d64180f7d0832d9fec2ef5a22cd
Kunaal Wadhwa <kunaalwadhwa@ucla.edu>
2013-02-25 19:02:50 -0800

    Add empty project files

---
+++ hipster-cow.cpp
@@ -0,0 +1,19 @@
+// C++ libraries
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// C libraries
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define MAXBUFFER 72000   // the maximum size of the buffer
+#define CONCURRENT 10
+
+using namespace std; 
+
+int main (int argc, char *argv[]){
+}
