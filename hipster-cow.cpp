a7176cca5ed91317e50d3145d609d5cd75bd06b9
Kunaal Wadhwa <kunaalwadhwa@ucla.edu>
2013-02-26 00:26:29 -0800

    Can now input target reads and reference genome

--- hipster-cow.cpp
+++ hipster-cow.cpp
@@ -3,32 +3,58 @@
 #include <fstream>
 #include <string>
 #include <cstring>
+#include <vector>
 
 #define REF_GENOME_IN "ref_genome.txt" // The file with the reference genome
+#define READ_TARGET "read_target.txt" // The file with the reads
 
 using namespace std;
 
-int input_ref_genome(string *buff) {
+int input_ref_genome (string *buff) {
 	string line;
-	ifstream myfile (REF_GENOME_IN);
-	if (myfile.is_open())
+	ifstream infile_ref_genome (REF_GENOME_IN);
+	if (infile_ref_genome.is_open())
 	{
-		while ( myfile.good())
+		while (infile_ref_genome.good())
 		{
-			getline (myfile, line);
+			getline (infile_ref_genome, line);
 			*buff = *buff + line + "\n";
 		}
-		myfile.close();
+		infile_ref_genome.close();
 		return 1;
 	}
 	return 0;
 }
 
+int input_target_reads (vector<string>& reads) {
 
+	
+	int numReads = 0;
+	string line;
+	
+	ifstream myfile (READ_TARGET);
+	if (myfile.is_open())
+	{
+		while ( myfile.good())
+		{
+			getline (myfile, line);
+			reads.push_back(line);
+			numReads++;
+		}
+		myfile.close();
+		return numReads;
+	}
+	return -1;
+}
 
 int main (int argc, char *argv[]) {
-	string refGenome ="";
+	string refGenome;
 	if(!input_ref_genome(&refGenome))
 		cerr << "Cannot open input file" << endl;
+	
+	vector<string> retReads;
+	input_target_reads (retReads);
+	cout << "First line is " << retReads[0] << endl;
+	cout << "Third line is " << retReads[2] << endl;
 	return 0;
 }
