cdace7ece56387d888f215296d0bee93e09e06d0
Kunaal Wadhwa <kunaalwadhwa@ucla.edu>
2013-02-28 23:09:51 -0800

    Upgraded to VS12 (threads)

--- hipster-cow.cpp
+++ hipster-cow.cpp
@@ -8,10 +8,12 @@
 #include <vector>
 #include <bitset>
 #include <stdio.h>
+#include <thread>
 
 #define REF_GENOME_IN "ref_genome.txt" // Path of file with the reference genome
 #define READ_TARGET "read_target.txt" // Path of file with the reads
-#define NUM_CHROM_TIMES_TWO 1200 // Number of chromosomes to be read (x2 for Bitset) [Set larger than expected]
+#define NUM_CHROM_TIMES_TWO 1200 // #chromosomes to be read (x2 for Bitset) [Set larger than expected]
+#define NUM_THREADS 10 // #threads spawned by repeat finder (reference)
 
 using namespace std;
 
@@ -21,9 +23,6 @@ typedef bitset<NUM_CHROM_TIMES_TWO> genome;
 //   PARAM:  Genome type (i.e. bitset of size of genome) passed by reference (returned)
 //   RETVAL: integer which returns size of genome when successful, -1 otherwise
 int input_ref_genome (genome & reference_genome) {
-	char inputChar;
-	//genome reference_genome;
-
 	FILE *file = fopen(REF_GENOME_IN, "r");
 
     size_t n = 0;
@@ -83,6 +82,25 @@ int input_target_reads (vector<string>& reads) {
 	return -1;
 }
 
+//int find_repeats_thread_fun(
+
+
+// **What does this function do? (TODO)
+//   PARAM:  Genome type (i.e. bitset of size of genome) passed by reference (returned)
+//   RETVAL: Fill this in later (TODO)
+int find_repeats (genome & reference_genome) {
+	vector<thread *> threadList;
+	for (int i = 0; i < NUM_THREADS; ++i)
+    //threadList.push_back(new thread(workerFunc));
+
+  for (int j = 0; j < NUM_THREADS; ++j)
+  {
+      threadList[j]->join();
+      delete threadList[j];
+  }
+	return 0;
+}
+
 
 // **Main function
 int main (int argc, char *argv[]) {
@@ -99,7 +117,6 @@ int main (int argc, char *argv[]) {
 	{
 		cout << refGenome[i];
 	}
-	//<< refGenome << endl << endl;
 	cout << "First line is " << retReads[0] << endl;
 	cout << "Third line is " << retReads[2] << endl;
 
